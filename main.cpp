4ad71cf9cdc6d663465a9b44d91158fba989ba84
Дмитрий <53074863+1Kuso4ek1@users.noreply.github.com>
2021-06-30 13:30:27 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,34 +1,36 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
+#include <fstream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
 
 void help() {  
-	std::cout << "Это меню помощи управления программой SERJ BUILD PROGRAMM. Ниже вы найдете команды программы. \n 1) help - помощь \n 2) assembly. Сразу после этой команды указать файл. Например: filename_programm.cpp - сборка программы  \n";  
+	std::cout << "Это меню помощи управления программой SERJ BUILD PROGRAM. Ниже вы найдете команды программы. \n 1) help - помощь \n 2) assembly. Сразу после этой команды указать файл. Например: filename_programm.cpp - сборка программы  \n"; 
+    exit(EXIT_SUCCESS);
 }
 
-bool find_comm(std::string com, int kolvo, char*argv[]) {
-	for (int i = 0; i < kolvo; i++) {
-		if (com == argv[i]) {
-			return true;
-		}
-	}
-	return false;
+void compile(std::string c) {
+    std::ifstream fin("INSTRUCTION.txt");
+    std::string tmp;
+    while (getline(fin, tmp)) {
+        system(tmp.c_str());
+    }
+    fin.close();
+    system(std::string("g++ " + c + " -o test && ./test").c_str());
+    std::cout << "\033[32mCompilation finished!\033[0m" << std::endl;
 }
 
 int main(int argc, char* argv[]) {
-	std::string list_com[] = { "help", "assembly" };
-	std::cout << "start program \n";
-	if (argc == 1 || find_comm(list_com[0], argc, argv)) {
-		help();
-		exit(EXIT_SUCCESS);
-	}
-	if (find_comm(list_com[1], argc, argv) && argc > 2) { 
-		std::string com = argv[2];
-		std::string out = "g++ " + com + " -o test";
-		system (out.c_str());    
-		system ("./test");                                   
-		std::cout << "\n Vozvrat: "  << "\n";
-	}
-	std::cout << "\033[32mCompilation finished!\033[0m" << std::endl;
+    std::cout << "\033[32mStart program\033[0m" << std::endl;
+    if(argv[1] == nullptr) help();
+	std::unordered_map<std::string, int> list_com = { { "help", 1 }, { "assembly", 2 } };
+    switch(list_com[argv[1]])
+    {
+    case 1: help(); break;
+    case 2: compile(argv[2]); break;
+    default: help(); break;
+    }
 	return 0;
 }
